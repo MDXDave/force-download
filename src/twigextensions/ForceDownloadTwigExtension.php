df7245456b415ce13f5ee4e11cbee75e41b48630
Dave <MDXDave@users.noreply.github.com>
2019-01-29 04:25:57 +0100

    Update ForceDownloadTwigExtension.php

--- src/twigextensions/ForceDownloadTwigExtension.php
+++ src/twigextensions/ForceDownloadTwigExtension.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * ForceDownload Plugin for Craft CMS 3.x
+ * ForceDownload Plugin for Craft CMS 3.1.x
  *
  * @link      https://mdxdave.de
- * @copyright Copyright (c) 2017 MDXDave
+ * @copyright Copyright (c) 2017-2019 MDXDave
  */
 
 namespace mdxdave\forcedownload\twigextensions;
@@ -40,76 +40,32 @@ class ForceDownloadTwigExtension extends \Twig_Extension
         $asset = Craft::$app->elements->getElementByUri($id);
         
       $path = $this->_getFullDirectoryPath($asset->filename[0]);
-      /*
-      <div class="ui grid">
-                <div class="column eight wide">
-                    <div class="item" style="white-space: nowrap">
-                        <div class="ui teal horizontal label">Dateiname</div>
-                        '. $asset->filename[0]->filename .'
-                    </div>
-                <div class="item">
-                    <div class="ui red horizontal label">Dateigröße</div>
-                </div>
-                </div>
-                <div class="column four wide">
-                <div class="item">
-                    <div class="ui green horizontal label">Downloads</div>
-                    
-                </div>
-                <div class="item">
-                    <div class="ui blue horizontal label">Dateityp&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
-                    
-                </div>
-                </div>
+      $output = '<br /><div class="ui grid center aligned">
+      <div class="ui raised compact segment olive" style="min-width: 70%;">
+        <h3 style="text-align: center">'. $asset->filename[0]->filename .'</h3>
+        <div class="ui three statistics mini">
+            <div class="statistic blue">
+                <div class="value">'. $formatter->asShortSize($asset->filename[0]->size) .'</div>
+                <div class="label">Größe</div>
             </div>
-            */
-          $output = '<br /><div class="ui grid center aligned">
-        <div class="ui raised compact segment olive" style="min-width: 70%;">
-            <h3 style="text-align: center">'. $asset->filename[0]->filename .'</h3>
-            <div class="ui three statistics mini">
-  <div class="statistic blue">
-    <div class="value">
-      '. $formatter->asShortSize($asset->filename[0]->size) .'
-    </div>
-    <div class="label">
-      Größe
-    </div>
-  </div>
-  <div class="statistic green">
-    <div class="value">
-      '. $asset->downloadCount .'
-    </div>
-    <div class="label">
-      Downloads
-    </div>
-  </div>
-  <div class="statistic">
-    <div class="value">
-      '. $asset->filename[0]->extension .'
-    </div>
-    <div class="label">
-      Dateityp
-    </div>
-  </div>
-</div>
-            ';
-            if($desc)
+            <div class="statistic green">
+                <div class="value">'. $asset->downloadCount .'</div>
+                <div class="label">Downloads</div>
+            </div>
+            <div class="statistic">
+                <div class="value">'. $asset->filename[0]->extension .'</div>
+                <div class="label">Dateityp</div>
+            </div>
+        </div>';
+        if($desc)
               $output .= '<p>'. $asset->text .'</p>';
-              
-
-      $output .= '<div class="ui grid center aligned">
-                <div class="column twelve wide" style="margin: 10px;">
-
+      $output .= '<div class="ui grid center aligned"><div class="column twelve wide" style="margin: 10px;">
     <form method="post" action="'. $asset->url .'?checksum='. hash("sha512", $asset->filename[0]->filename) .'&do=download">
         <input type="hidden" name="'. Craft::$app->config->general->csrfTokenName .'" value="'. Craft::$app->request->csrfToken .'">
         <button id="downloadButton" class="ui labeled icon button green big">
             <i class="download icon white"></i> Herunterladen
         </button>
-    </form></div>
-
-            </div>
-        </div>
-    </div><br />';
+    </form></div></div></div></div><br />';
     return \Craft\helpers\Template::raw($output);
      
     }
@@ -118,7 +74,6 @@ class ForceDownloadTwigExtension extends \Twig_Extension
         $raw = preg_replace_callback("/\[download\]([0-9a-z-\/]*)\[\/download\]/", function($match){
           return $this->showDownloadButton($match[1], true);
         }, $raw);
-        
         return $raw;    
     }
 
@@ -144,6 +99,9 @@ class ForceDownloadTwigExtension extends \Twig_Extension
     {
         $volumeId = $file->volumeId;
         $volume = Craft::$app->getVolumes()->getVolumeById($volumeId);
-        return $volume->path;
+        if($volume->url != null)
+          return $volume->url;
+        else
+          return $volume->path;
     }
 }
